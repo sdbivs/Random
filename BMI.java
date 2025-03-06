efd57f4c8cb2521e5bae06ee213ca43e9eb31dd0
Sharr Ismaili <129428272+sdbivs@users.noreply.github.com>
2025-03-06 02:16:12 +0100

    Translated BMI.java
    Translated BMI from Albanian to English for easier understanding of code

--- BMI.java
+++ BMI.java
@@ -3,21 +3,21 @@ public class BMI{
 	public static void main(String[] args){
 		Scanner scanner = new Scanner(System.in);
 		
-		System.out.println("Shkruajeni peshen e juaj:");
+		System.out.print("Type your wight: ");
 		float kg = scanner.nextFloat();
-		System.out.println("Shkruajeni gjatesine e juaj:");
+		System.out.print("Type your height: ");
 		float m = scanner.nextFloat();
 		
 		float res = (float)kg / (float)(Math.pow(m, 2));
 		
 		if(res < 18.5){
-			System.out.println("Nen peshen e rekomanduar");
+			System.out.println("Under the recommended weight");
 		}else if(res >= 18.5 && res <= 24.9){
-			System.out.println("Ne peshen e rekomanduar");
+			System.out.println("In the recommended weight");
 		}else if(res >= 25.0 && res <= 29.9){
-			System.out.println("Mbi peshen e rekomanduar");
+			System.out.println("Over the recommended weight");
 		}else{
-			System.out.println("Obes");
+			System.out.println("Obese");
 		}
 		
 		System.out.println(res);
